17deecec9369a52c9db1ed4d6dc1f7eca56e96d7
Unknown <rm.hobbel@telfort.nl>
2017-11-24 15:38:42 +0100

    Updatet to Java 1.8

--- app/src/main/java/nl/arthurtech/hueapp/LampListActivity.java
+++ app/src/main/java/nl/arthurtech/hueapp/LampListActivity.java
@@ -2,12 +2,30 @@ package nl.arthurtech.hueapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.List;
 
 public class LampListActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    LampRecyclerViewAdapter lampRecyclerViewAdapter;
+    List<LampItem> lamps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lamp_list);
+
+        GetLamps();
+        recyclerView =findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        lampRecyclerViewAdapter = new LampRecyclerViewAdapter(this, lamps);
+        recyclerView.setAdapter(lampRecyclerViewAdapter);
+    }
+
+    //Moet de lampen in de lijst met lampitems stoppen
+    private void GetLamps() {
     }
 }
--- app/src/main/java/nl/arthurtech/hueapp/MainActivity.java
+++ app/src/main/java/nl/arthurtech/hueapp/MainActivity.java
@@ -2,22 +2,24 @@ package nl.arthurtech.hueapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
 {
-    public LampCommunication lampCommunication;
-
+    Button avansButton;
+    Button emulatorButton;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        avansButton = findViewById(R.id.buttonReal);
+        emulatorButton = findViewById(R.id.buttonSimulator);
 
-        lampCommunication = new LampCommunication();
-    }
+        avansButton.setOnClickListener((View v) -> {
+            
+        });
 
-    public LampCommunication getLampCommunication()
-    {
-        return lampCommunication;
     }
 }
