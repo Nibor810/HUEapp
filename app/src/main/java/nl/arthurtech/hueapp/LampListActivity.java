a2952a2fbbf3b029f8bae8abe843be4f90c4e99c
Unknown <rm.hobbel@telfort.nl>
2017-11-29 13:05:19 +0100

    Added Code to read string from intent

--- app/src/main/java/nl/arthurtech/hueapp/LampListActivity.java
+++ app/src/main/java/nl/arthurtech/hueapp/LampListActivity.java
@@ -1,5 +1,6 @@
 package nl.arthurtech.hueapp;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.net.Uri;
 import android.support.annotation.NonNull;
@@ -18,20 +19,28 @@ import java.util.ListIterator;
 public class LampListActivity extends AppCompatActivity implements ListFragment.OnFragmentInteractionListener, LampDetailFragment.OnFragmentInteractionListener {
     //RecyclerView recyclerView;
     //LampRecyclerViewAdapter lampRecyclerViewAdapter;
-    List<LampItem> lamps= new ArrayList<>();
+    List<LampItem> lamps = new ArrayList<>();
     private static final String TAG = "LampListActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_lamp_list);
+        String apiKey;
+        Bundle extras = getIntent().getExtras();
+        if (extras == null) {
+            //TODO: what to do with empty key.
+        } else {
+            apiKey = extras.getString("APIKEY");
+        }
         //TODO: Create Lamp Communication
     }
 
     @Override
     public void onFragmentInteraction(LampItem lamp) {
         LampDetailFragment lampDetailFragment = (LampDetailFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_lamp_list);
-        Log.i(TAG,"ListitemClicked");
+        Log.i(TAG, "ListitemClicked");
         lampDetailFragment.setLamp(lamp);
     }
 
