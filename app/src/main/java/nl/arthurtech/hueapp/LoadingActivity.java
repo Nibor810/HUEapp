45a63c68f7b7a91aded0d27f40ed7a830c41d294
Unknown <rm.hobbel@telfort.nl>
2017-11-29 14:42:22 +0100

    Updated Request to be able to have a body

--- app/src/main/java/nl/arthurtech/hueapp/LoadingActivity.java
+++ app/src/main/java/nl/arthurtech/hueapp/LoadingActivity.java
@@ -7,7 +7,11 @@ import android.util.Log;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -19,40 +23,14 @@ public class LoadingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
 
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, "http://145.49.19.85:80/api",
-                new Response.Listener<String>()
-                {
-                    @Override
-                    public void onResponse(String r)
-                    {
-                        System.out.println(r);
-
-                        //TODO: Make seperate Request for API Key.
-                        //TODO: CALL CALLBACK LOADING ACTIVITY
-                    }
-                },
-                new Response.ErrorListener()
-        {
-
-            @Override
-            public void onErrorResponse(VolleyError error)
-            {
-                Log.d("ERROR", "API CALL ERROR");
-                Log.d("ERROR", error.getMessage());
-            }
-        })
-        {
-            /* Possible PARAMS.
-            @Override
-            protected Map<String, String> getParams()
-            {
-                Map<String, String> params = new HashMap<String, String>();
-                params.put("devicetype", "Phone");
-
-                return params;
-            }*/
-        };
-
-        MyVolleyRequestQueue.getInstance(this).getRequestQueue().add(stringRequest);
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.POST, "", null, r -> {
+            System.out.println(r);
+            //TODO: Make seperate Request for API Key.
+            //TODO: CALL CALLBACK LOADING ACTIVITY
+        }, error -> {
+            Log.d("ERROR", "API CALL ERROR");
+            Log.d("ERROR", error.getMessage());
+        });
+        MyVolleyRequestQueue.getInstance(this).getRequestQueue().add(request);
     }
 }
