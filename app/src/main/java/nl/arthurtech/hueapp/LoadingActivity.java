3304c47f44e1ad3576ca98ab2d5378542715d2f7
Arthur van Strien <arthurvanstrien@gmail.com>
2017-11-29 14:29:47 +0100

    Added a request for the API key.

--- app/src/main/java/nl/arthurtech/hueapp/LoadingActivity.java
+++ app/src/main/java/nl/arthurtech/hueapp/LoadingActivity.java
@@ -2,6 +2,15 @@ package nl.arthurtech.hueapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoadingActivity extends AppCompatActivity {
 
@@ -9,8 +18,41 @@ public class LoadingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, "http://145.49.19.85:80/api",
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String r)
+                    {
+                        System.out.println(r);
+
+                        //TODO: Make seperate Request for API Key.
+                        //TODO: CALL CALLBACK LOADING ACTIVITY
+                    }
+                },
+                new Response.ErrorListener()
+        {
+
+            @Override
+            public void onErrorResponse(VolleyError error)
+            {
+                Log.d("ERROR", "API CALL ERROR");
+                Log.d("ERROR", error.getMessage());
+            }
+        })
+        {
+            /* Possible PARAMS.
+            @Override
+            protected Map<String, String> getParams()
+            {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("devicetype", "Phone");
+
+                return params;
+            }*/
+        };
+
+        MyVolleyRequestQueue.getInstance(this).getRequestQueue().add(stringRequest);
     }
-
-
-
 }
