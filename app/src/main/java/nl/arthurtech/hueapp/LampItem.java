c79b3a48a55c8762456b7a1dde6bb435a37db07f
Unknown <rm.hobbel@telfort.nl>
2017-12-05 14:39:38 +0100

    Possible Fix

--- app/src/main/java/nl/arthurtech/hueapp/LampItem.java
+++ app/src/main/java/nl/arthurtech/hueapp/LampItem.java
@@ -39,8 +39,8 @@ public class LampItem {
     }
 
     public int getColor(){
-        Log.i("COLOR",String.valueOf(Color.HSVToColor(new float[]{lampHue/254,brightness/254,saturation/65535})));
-        return Color.HSVToColor(new float[]{lampHue/254,brightness/254,saturation/65535});
+        Log.i("COLOR",String.valueOf(Color.HSVToColor(new float[]{lampHue/65535,brightness/254,saturation/254})));
+        return Color.HSVToColor(new float[]{lampHue/65535,brightness/254,saturation/254});
     }
 
 
